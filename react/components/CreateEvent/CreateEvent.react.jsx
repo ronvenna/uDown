c78e68ef1783e7c7b2143627db1d6ae0b743f1b5
Dan Zhang <dyz2@duke.edu>
2016-02-27 23:51:09 -0800

    Push add event modal

--- react/components/CreateEvent/CreateEvent.react.jsx
+++ react/components/CreateEvent/CreateEvent.react.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom'
 class CreateEvent extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      saving: false,
+      savingText: "Save"
+    }
+  }
+
+  saveEvent() {
+    this.setState({
+      savingText: ""
+    })
+    this.props.close();
+  }
 
   render() {
     let newEventHeader = "Create new event";
-
+    let saving = this.state.saving ? "is-loading" : "";
     return(
-      <div className="column is-4 is-offset-4">
+      <div className="">
         <div className="event">
           <div className="event_header">
             {newEventHeader}
           </div>
-          <div className="event_body">
-            <p className="eventLabel">Event: </p><input className="inputEvent" type="text"></input>
-            <p className="eventLabel">Time: </p><input className="inputEvent" type="text"></input>
-            <p>Select Slack team members:</p>
-            <input className="inputUsers" type="text"></input>
-            <button className="button eventSave pull-right">Save</button>
+          <div className="event_body create_event">
+            <div className="columns">
+              <span className="eventLabel column is-2">Event: </span>
+              <input className="eventInput column is-10" type="text"></input>
+            </div>
+            <div className="columns">
+              <span className="eventLabel column is-2">Time: </span>
+              <input className="eventInput column is-10" type="text"></input>
+            </div>
+            <div className="columns">
+              <span className="eventLabel column is-2">Invite: </span>
+              <textarea className="eventInput column is-12" type="text"></textarea>
+            </div>
+
+            <a ref="save" className={"button eventSave is-pulled-right " + saving} onClick={this.saveEvent.bind(this)}>{this.state.savingText}</a>
+
           </div>
         </div>
       </div>
--- react/components/Main.react.jsx
+++ react/components/Main.react.jsx
@@ -1,144 +1,54 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Header from './Header/Header.react.jsx';
 import EventList from './Events/EventList.react.jsx';
 import CreateEvent from './CreateEvent/CreateEvent.react.jsx';
 import PlusButton from './CreateEvent/PlusButton.react.jsx';
-let data = {
-  events : [
-    {
-      name: "Lunch",
-      location: "Chipotle",
-      time: "1pm",
-      attendees: [
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        }
-      ],
-      invited: [
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        }
-      ]
-    },
-    {
-      name: "Lunch",
-      location: "Chipotle",
-      time: "1pm",
-      attendees: [
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        }
-      ],
-      invited: [
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        },
-        {
-          name: "Jane Gonzales",
-          image: "http://img06.deviantart.net/3c50/i/2005/096/e/3/minimalist_headshot_stock_2_by_kristyvictoria.jpg",
-          team: "Strongpoop"
-        }
-      ]
-    }
-  ]
-};
 
 class Main extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      newEventActive : false,
+      modal: <CreateEvent close={this.closeEvent.bind(this)} />
+    }
+  }
+
   addNewEvent() {
-      console.log("add me boi");
+    this.setState({
+      newEventActive : true,
+      modal: <CreateEvent close={this.closeEvent.bind(this)} />
+    })
+  }
+
+  closeEvent() {
+    this.setState({
+      newEventActive : false,
+      modal: null
+    })
   }
 
   render() {
-
+    let isActive = this.state.newEventActive ? "is-active" : "";
     return(
       <div className="">
         <Header />
+        <div className={"modal " + isActive}>
+          <div className="modal-background"></div>
+          <div className="modal-container">
+            <div ref="modalContent" className="modal-content">
+              {this.state.modal}
+            </div>
+          </div>
+          <button className="modal-close" onClick={this.closeEvent.bind(this)}></button>
+        </div>
         <div className="columns">
           <EventList events={data.events}/>
           <div className="column is-1 is-offset-3 add_event">
-            <PlusButton onClick={this.addNewEvent}/>
+            <PlusButton onClick={this.addNewEvent.bind(this)}/>
           </div>
         </div>
-        <CreateEvent />
       </div>
     );
   }
